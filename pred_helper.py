6de70c056596edc32ee31c9d24d2c409af4defdc
Naveen31-4 <Naveendevarla418@gmail.com>
2025-04-23 22:04:20 -0500

    Switched to relative paths for model and scaler loading

--- pred_helper.py
+++ pred_helper.py
@@ -1,14 +1,13 @@
-# codebasics ML course: codebasics.io, all rights reserverd
 
 import pandas as pd
 import joblib
 
-model_rest = joblib.load(r"C:\Users\Naveen\OneDrive\Desktop\Projects\app\artifacts\model_rest.joblib")
-model_young = joblib.load(r"C:\Users\Naveen\OneDrive\Desktop\Projects\app\artifacts\model_young.joblib")
+model_rest = joblib.load("artifacts/model_rest.joblib")
+model_young = joblib.load("artifacts/model_young.joblib")
 
 # Load scalers
-scaler_rest = joblib.load(r"C:\Users\Naveen\OneDrive\Desktop\Projects\app\artifacts\scaler_rest.joblib")
-scaler_young = joblib.load(r"C:\Users\Naveen\OneDrive\Desktop\Projects\app\artifacts\scaler_young.joblib")
+scaler_rest = joblib.load("artifacts/scaler_rest.joblib")
+scaler_young = joblib.load("artifacts/scaler_young.joblib")
 
 def calculate_normalized_risk(medical_history):
     risk_scores = {
